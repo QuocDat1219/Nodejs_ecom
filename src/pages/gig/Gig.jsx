c4b3548695941fd078cb5c95a234b7001646dcf4
QuocDung21 <rongvang2357hn@gmail.com>
2023-02-24 20:22:04 +0700

    new backend

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Gig = () => {
-    return (
-        <div className='gig'>Gig thay doi nua ne moi moi moiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</div>
-    )
+   return (
+      <div className="gig">
+         Gig thay doi nua ne moi moi
+         moiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+         <div>Backend</div>
+      </div>
+   )
 }
 
 export default Gig
