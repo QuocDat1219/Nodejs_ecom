0ee7142855784d73efac0f67800bd1371039c3a2
QuocDung21 <rongvang2357hn@gmail.com>
2023-02-24 20:18:04 +0700

    new

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Gig = () => {
-    return (
-        <div className='gig'>Gig thay doi nua ne moi moi moiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</div>
-    )
+   return (
+      <div className="gig">
+         Gig thay doi nua ne moi
+         <div>moi moiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</div>
+      </div>
+   )
 }
 
 export default Gig
