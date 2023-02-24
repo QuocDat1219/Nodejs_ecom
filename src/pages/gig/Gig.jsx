f025e205638263d41084e9a71357b0ae839e3519
QuocDung21 <rongvang2357hn@gmail.com>
2023-02-24 19:33:16 +0700

    first commit

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Gig = () => {
     return (
-        <div className='gig'>Gig thay doi nua ne</div>
+        <div className='gig'>Gig thay doi nua ne moi moi moi</div>
     )
 }
 
